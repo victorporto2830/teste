8269f83c93520f2704423bdc92d6cce95743f730
Victor Osorio <victor.osorio@souunit.com.br>
2022-06-14 13:06:19 -0300

    master

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get(`/`, (req, res)=>{
+    res.send('Bem vindo a API')
+})
+
+app.get('/:nome', (req, res) => {
+    nome = req.params.nome;
+    res.status(200).json({mensagem: `Bem vindo à API ${nome}`})
+})
+
+app.listen(port, ()=> {
+    console.log(`Servidor rodando na porta ${port}`);
+});
