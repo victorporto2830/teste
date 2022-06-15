140231ea402c3ae449b609c06b13b4f3a825ffd8
Victor Osorio <victor.osorio@souunit.com.br>
2022-06-15 13:10:13 -0300

    master

--- controller/PessoaController.js
+++ controller/PessoaController.js
@@ -6,11 +6,8 @@ class PessoaController {
 
   static ListarPessoas = (req, res) => {
       db.query(`SELECT * FROM Pessoa`, (erro, result) => {
-        if (erro) {
-          console.log(erro);
-        }else {
-          res.status(200).json(result) ;
-      };
+        if (erro) console.log(erro);
+        else res.status(200).json(result);
     });
   };
 
@@ -22,11 +19,18 @@ class PessoaController {
   };
 
   static InserirPessoa = (req, res) => {
-    db.query(`INSERT INTO Pessoa ('Nome') VALUES (?)`, [req.body.nome], (erro, result) => {
+    db.query(`INSERT INTO Pessoa (Nome) VALUES ('${req.body.nome}')`, (erro, result) => {
       if(erro) console.log(erro);
       else res.status(201).json({mensagem: "Pessoa cadastrada com sucesso"});
     });
   };
+
+  static DeletarPessoa = (req, res) => {
+    db.query(`DELETE FROM Pessoa WHERE idPessoa = ${req.params.id}`, (erro, result) => {
+      if(erro) console.log(erro)
+      else res.status(200).json({mensagem: "Pessoa deletada com sucesso"})
+    })
+  }
 };
 
 module.exports = PessoaController;
--- router/routerPessoa.js
+++ router/routerPessoa.js
@@ -6,5 +6,6 @@ const PessoaController = require("../controller/PessoaController")
 router.get("/:id", PessoaController.ListarPessoasPorId);
 router.get("/", PessoaController.ListarPessoas);
 router.post("/", PessoaController.InserirPessoa);
+router.delete("/:id", PessoaController.DeletarPessoa);
 
 module.exports = router;
