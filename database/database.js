12acd39c4c1f19efb17b909b9cacbafa508109ef
Victor Osorio <victor.osorio@souunit.com.br>
2022-06-15 12:44:46 -0300

    master

--- database/database.js
+++ database/database.js
@@ -1,12 +1,6 @@
+require('dotenv').config()
 const mysql = require("mysql2");
 
-const database = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "senha",
-    database: "Teste"
-  }) 
-  || 
-  mysql.createPool(process.env.DATABASE_URL);
-  
+const database = mysql.createPool(process.env.DATABASE_URL);
+
 module.exports = database;
