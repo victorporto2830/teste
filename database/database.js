1f73eb27e91a1915b9d55446cc42f58d53282aa3
Victor Osório <victorporto_@hotmail.com>
2022-06-18 18:24:20 -0300

    master

--- database/database.js
+++ database/database.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const mysql = require("mysql2");
+const pg = require('pg')
 
-const database = mysql.createPool(process.env.DATABASE_URL);
+const pool = new pg.Pool({ connectionString: process.env.HEROKU_POSTGRESQL_JADE_URL });
+const database = pool.connect();
+
+//const databasee = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = database;
